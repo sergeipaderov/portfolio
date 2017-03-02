bfdf512a220346e5fbb97a6c9f0506927a8d8d83
Sergei Paderov <sergeipaderov@gmail.com>
2017-03-02 16:59:13 -0300

    animation

---
+++ js/common.js
@@ -0,0 +1,8 @@
+/**
+* Created by SergeiPaderov on 02.03.2017.
+*/
+
+window.onload = function () {
+  var helloPage = document.querySelector('.hello');
+  hello.classList.add('hello_animation');
+};
